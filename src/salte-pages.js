bbcea074e637f1be72bb83c42bf1eed9b9d7cfb2
Ceci <admin@cecilias.me>
2019-03-03 12:17:58 -0600

    docs: updated readme and added issue template
    * removed unnecessary if check

--- src/salte-pages.js
+++ src/salte-pages.js
@@ -41,64 +41,62 @@ class SaltePages extends LitElement {
   }
 
   updated(changedProperties) {
-    if (changedProperties) {
-      if (changedProperties.has('selected')) {
-        this.loading = true;
-  
-        /** @type {Promise<void>} */
-        this.loadComplete = Promise.resolve().then(() => {
-          const { page, element } = this._query(this.selected, this.fallback);
-  
-          if (!element) {
-            throw new Error(`The given pages do not exist. (selected: ${this.selected}, fallback: ${this.fallback})`);
-          }
-  
-          const event = new CustomEvent('load', {
-            detail: page
-          });
-          this.dispatchEvent(event);
-  
-          const promises = [];
-          if (element.tagName.match(/^\w+-/i)) {
-            promises.push(window.customElements.whenDefined(element.tagName.toLowerCase()));
-          }
-  
-          return Promise.all(promises).then(() => {
-            const previouslySelected = this.querySelector('[selected]');
-  
-            const promises = [];
-            if (previouslySelected) {
-              promises.push(Promise.resolve(previouslySelected.hide && previouslySelected.hide()).then(() => {
-                previouslySelected.removeAttribute('selected');
-              }));
-            }
-  
-            return Promise.all(promises).then(() => {
-              element.setAttribute('selected', '');
-  
-              return Promise.resolve(element.show && element.show(!!previouslySelected));
-            }).then(() => {
-              const event = new CustomEvent('loaded', {
-                detail: page
-              });
-              this.dispatchEvent(event);
-  
-              // Wait for the next render...
-              return this._renderComplete;
-            });
-          })
-        }).then(() => {
-          this.loading = false;
-        }).catch((error) => {
-          this.loading = false;
-          console.error(error);
-          const event = new CustomEvent('load-failed', {
-            detail: this.selected
-          });
-          this.dispatchEvent(event);
-          return Promise.reject(error);
+    if (changedProperties.has('selected')) {
+      this.loading = true;
+
+      /** @type {Promise<void>} */
+      this.loadComplete = Promise.resolve().then(() => {
+        const { page, element } = this._query(this.selected, this.fallback);
+
+        if (!element) {
+          throw new Error(`The given pages do not exist. (selected: ${this.selected}, fallback: ${this.fallback})`);
+        }
+
+        const event = new CustomEvent('load', {
+          detail: page
         });
-      }
+        this.dispatchEvent(event);
+
+        const promises = [];
+        if (element.tagName.match(/^\w+-/i)) {
+          promises.push(window.customElements.whenDefined(element.tagName.toLowerCase()));
+        }
+
+        return Promise.all(promises).then(() => {
+          const previouslySelected = this.querySelector('[selected]');
+
+          const promises = [];
+          if (previouslySelected) {
+            promises.push(Promise.resolve(previouslySelected.hide && previouslySelected.hide()).then(() => {
+              previouslySelected.removeAttribute('selected');
+            }));
+          }
+
+          return Promise.all(promises).then(() => {
+            element.setAttribute('selected', '');
+
+            return Promise.resolve(element.show && element.show(!!previouslySelected));
+          }).then(() => {
+            const event = new CustomEvent('loaded', {
+              detail: page
+            });
+            this.dispatchEvent(event);
+
+            // Wait for the next render...
+            return this._renderComplete;
+          });
+        })
+      }).then(() => {
+        this.loading = false;
+      }).catch((error) => {
+        this.loading = false;
+        console.error(error);
+        const event = new CustomEvent('load-failed', {
+          detail: this.selected
+        });
+        this.dispatchEvent(event);
+        return Promise.reject(error);
+      });
     }
   }
 
